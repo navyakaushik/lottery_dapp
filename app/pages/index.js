95afc75d45eea3ba33d8ab2630b8e39f1d5a86eb
navyakaushik <navyakaushik678@gmail.com>
2024-09-02 21:12:39 +0530

    initial commit

--- app/pages/index.js
+++ app/pages/index.js
@@ -3,6 +3,10 @@ import PotCard from "../components/PotCard";
 import Table from "../components/Table";
 import style from "../styles/Home.module.css";
 
+import {useMemo} from "react";
+import {ConnectinProvider, WalletProvider} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+
 export default function Home() {
   return (
     <div className={style.wrapper}>
